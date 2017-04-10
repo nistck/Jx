79ff149c05d12daf0a5387b9f9502a71dbead582
panjincai <nistck@126.com>
2017-04-10 17:55:26 +0800

    Ext

---
+++ Jx/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jx
+{
+    public static class Log
+    {
+        public static void Fatal(string message, params object[] args)
+        {
+
+        }
+
+        public static void Error(string message, params object[] args)
+        {
+
+        }
+
+        public static void Warning(string message, params object[] args)
+        {
+
+        }
+    }
+}
